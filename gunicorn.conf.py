03c2b36f397c18c210e1c6fbe4f80e0611ee0902
axmetmercan <axmetmercan@outlook.com>
2024-03-21 20:05:04 +0100

    add docker-files

--- gunicorn.conf.py
+++
@@ -1,4 +0,0 @@
-bind = '0.0.0.0:8000'  # This should match the port specified in your Dockerfile CMD
-workers = 3  # Number of worker processes
-threads = 3  # Number of worker threads per worker process
-timeout = 60  # Timeout for worker processes
